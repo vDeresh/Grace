72623d76af331b44d063ec3dfaa2f9ca696a32d5
vDeresh <106701009+vDeresh@users.noreply.github.com>
2022-12-24 01:16:43 +0100

    Update instl.bat

--- Grace/dwnl/instl.bat
+++ Grace/dwnl/instl.bat
@@ -3,6 +3,8 @@ pip install SpeechRecognition
 pip install pyttsx3
 pip install os-sys
 pip install DateTime
+pip install Wikipedia 
+pip install PyAudio
 @echo off
 cd msg
 start powodzenieInstalacja.vbs
