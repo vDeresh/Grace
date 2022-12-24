ac3ecb011ffb10154e32ecd5dd36d25233977029
vDeresh <106701009+vDeresh@users.noreply.github.com>
2022-12-24 01:15:44 +0100

    Update instl.bat

--- Grace/dwnl/instl.bat
+++ Grace/dwnl/instl.bat
@@ -1,14 +1,8 @@
-@timeout 1
 pip install Wheel
-@timeout 1
 pip install SpeechRecognition
-@timeout 1
 pip install pyttsx3
-@timeout 1
 pip install os-sys
-@timeout 1
 pip install DateTime
-@timeout 1
 @echo off
 cd msg
 start powodzenieInstalacja.vbs
